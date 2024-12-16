9f8a7b152f205201d1b039c1d270805f5e201b75
cristhiamdaniel-cj <crcampos@javeriana.edu.co>
2024-12-16 15:27:49 +0000

    Excluidos archivos sensibles del control de versiones

--- taskmanager/data_seed.py
+++ taskmanager/data_seed.py
@@ -1,15 +1,24 @@
 from django.contrib.auth.models import User
+from decouple import config  # Importa la función config de python-decouple
 
+# Datos de usuarios
 users_data = [
-    {'username': 'danielamazuera', 'password': 'KENNEDY2024.', 'first_name': 'Daniela', 'last_name': 'Mazuera'},
-    {'username': 'sofiapuerto', 'password': 'KENNEDY2024.', 'first_name': 'Sofia', 'last_name': 'Puerto'},
-    {'username': 'danielcampos', 'password': 'KENNEDY2024.', 'first_name': 'Daniel', 'last_name': 'Campos'},
+    {'username': 'danielamazuera', 'password': config('USER1_PASS'), 'first_name': 'Daniela', 'last_name': 'Mazuera'},
+    {'username': 'sofiapuerto', 'password': config('USER2_PASS'), 'first_name': 'Sofia', 'last_name': 'Puerto'},
+    {'username': 'danielcampos', 'password': config('SUPERUSER_PASS'), 'first_name': 'Daniel', 'last_name': 'Campos'},
 ]
 
+# Crear usuarios
 for user_data in users_data:
     user, created = User.objects.get_or_create(username=user_data['username'])
     if created:
         user.set_password(user_data['password'])
         user.first_name = user_data['first_name']
         user.last_name = user_data['last_name']
+        if user_data['username'] == 'danielcampos':  # Hace que danielcampos sea superusuario
+            user.is_superuser = True
+            user.is_staff = True
         user.save()
+        print(f"Usuario {user_data['username']} creado correctamente.")
+    else:
+        print(f"El usuario {user_data['username']} ya existe.")
